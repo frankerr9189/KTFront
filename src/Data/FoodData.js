725a539e059497cfd82d6f7a97467562d7effdd9
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-07-08 17:36:17 -0400

    menu

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,22 +1,110 @@
-export const foods =[
+export const foodItems =[
     {
     name: 'Pizza',
-    img: '/img/pizza.jpeg'
+    img: '/img/pizza.jpeg',
+    category: 'Pizza'
     },
     {
-    name: 'CheeseBurger',
-    img: '/img/cheeseburger.jpeg'
+        name: 'Cheese Pizza',
+        img: '/img/cheesepizza.jpeg',
+        category: 'Pizza'
+        },
+    {
+        name: 'Pepperoni Pizza',
+        img: '/img/pepperonipizza.jpeg',
+        category: 'Pizza'
+     },
+     {
+        name: 'Sausage Pizza',
+        img: '/img/sausagepizza.jpeg',
+        category: 'Pizza'
     },
     {
-    name: 'Chicken Fingers',
-    img: '/img/fingerfoods.jpeg'
+        name: 'Shrimp Pizza',
+        img: '/img/shrimppizza.jpeg',
+        category: 'Pizza'
     },
     {
-    name: 'Sandwich',
-    img: '/img/sandwich.jpeg'
+    name: 'Cheese Burger',
+    img: '/img/cheeseburger.jpeg',
+    category: 'Burgers'
+    },
+    {
+    name: 'Cheese Burger',
+    img: '/img/cheeseburger2.jpeg',
+    category: 'Burgers'
+    },
+    {
+    name: 'Bacon Cheese Burger',
+    img: '/img/baconburger.jpeg',
+    category: 'Burgers'
+    },
+    {
+        name: 'Egg Rolls',
+        img: '/img/eggrolls.jpeg',
+        category: 'Appetizer'
     },    
     {
-    name: 'Drinks',
-    img: '/img/drinks.jpeg'
+    name: 'Chicken Fingers',
+    img: '/img/fingerfoods.jpeg',
+    category: 'Appetizer'
     },
-]
+    {
+    name: 'Mozzarella Sticks',
+    img: '/img/mozzarellasticks.jpeg',
+    category: 'Appetizer'
+    },
+    {
+        name: 'Wings',
+        img: '/img/wings.jpeg',
+        category: 'Appetizer'
+        },
+    {
+    name: 'Sandwich',
+    img: '/img/sandwich.jpeg',
+    category: 'Sandwich'
+    },  
+    {
+        name: 'Cheese Steak',
+        img: '/img/cheesesteak.jpeg',
+        category: 'Sandwich'
+  },    
+  {
+    name: 'Buffalo Chicken Sandwich',
+    img: '/img/buffalochickensandwich.jpeg',
+    category: 'Sandwich'
+    },  
+    {
+        name: 'Italian Hoagie',
+        img: '/img/italianhoagie.jpeg',
+        category: 'Sandwich'
+        },  
+    {
+    name: 'Drinks',
+    img: '/img/drinks.jpeg',
+    category: 'Drinks'
+    },
+    {
+    name: 'Red Wine',
+    img: '/img/redwine.jpeg',
+    category: 'Drinks'
+    },
+    {
+    name: 'Martini',
+    img: '/img/martini.jpeg',
+    category: 'Drinks'
+    },
+    {
+        name: 'Cape May IPA',
+        img: '/img/capemayipa.jpeg',
+        category: 'Drinks'
+    },
+];
+
+export const foods = foodItems.reduce((res, food) => {
+    if(!res[food.category]){
+        res[food.category]=[];
+    }
+    res[food.category].push(food);
+    return res;
+}, {});
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -8,13 +8,20 @@ const MenuStyled = styled.div`
     margin: 0px 400px 50px 20px;
 `
 export function Menu(){
-    return<MenuStyled>
-        <h1>Menu</h1>
+    return(
+    <MenuStyled>
+        {Object.entries(foods).map(([sectionName, foods]) => (
+            <>
+        <h1>{sectionName}</h1>
         <FoodGrid>
         {foods.map(food =>(
             <Food img={food.img}>
-                <FoodLabel>{food.name}</FoodLabel></Food>
+                <FoodLabel>{food.name}</FoodLabel>
+                </Food>
         ))}
         </FoodGrid>
+        </>
+        ))}
     </MenuStyled>
+    );
 }
