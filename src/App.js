a2e6f338d79433faaeccf8ec3f29282bc90c0199
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-07-08 13:01:05 -0400

    update fonts and global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
+    <>
+    <GlobalStyle/>
    <div>Ordering App</div>
+   <h1>Test</h1>
+   </>
   );
 }
 
