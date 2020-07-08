fc96556fd002798ddf2df81966030af8d8319d24
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-07-08 17:52:40 -0400

    css transitions

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -22,13 +22,14 @@ background-size: cover;
 filter: contrast(70%);
 border-radius: 7px;
 margin-top: 5px;
-transition-property: box-shadow margin-top;
-transition-duration: 1s;
+transition-property: box-shadow margin-top filter;
+transition-duration: .2s;
 box-shadow: 0px 0px 4px 0px grey;
 &:hover {
     cursor: pointer;
-    filter: contrast(100%)
+    filter: contrast(100%);
     margin-top: 0px;
-    box-shadow: 0px 0px 15px 0px grey;
+    margin-bottom: 5px;
+    box-shadow: 0px 5px 15px 0px grey;
 }
 `
