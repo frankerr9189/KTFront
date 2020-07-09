75c8663c5ac202e231439ff05c42a213cf6fdf44
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-07-08 23:50:40 -0400

    buttonsorderdialog

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import {FoodLabel} from "../Menu/FoodGrid";
+import {seaBlue} from '../Styles/colors';
+import {Title} from '../Styles/title';
 
 const Dialog = styled.div`
     width: 500px;
-    height: 2000px;
     background-color: white;
     position: fixed;
     top: 75px;
     z-index: 5;
     max-height: calc(100% - 100px);
     left: calc(50% - 250px);
+    display: flex;
+    flex-direction: column;
 `;
 const DialogShadow = styled.div`
     position: fixed;
@@ -35,6 +38,30 @@ const DialogBannerName = styled(FoodLabel)`
     font-size: 30px;
     padding: 5px 40px;
 `;
+//overflow auto adds the scroll bar to the dialog content area if needed
+export const DialogContent = styled.div`
+overflow: auto; 
+min-height: 100px;
+`;
+
+export const DialogFooter = styled.div`
+box-shadow: 0px -2px 10px 0px grey;
+height: 60px;
+display: flex;
+justify-content: center;
+`;
+
+export const ConfirmButton = styled(Title)`
+margin: 10px;
+color: white;
+height: 20px;
+border-radius: 5px;
+padding: 10px;
+text-align: center;
+width: 200px;
+cursor: pointer;
+background-color: ${seaBlue};
+`;
 
 export function FoodDialog({openFood, setOpenFood}){
     function close() {
@@ -49,6 +76,12 @@ export function FoodDialog({openFood, setOpenFood}){
     <DialogBanner img={openFood.img}>
         <DialogBannerName>{openFood.name}</DialogBannerName>
     </DialogBanner>
+    <DialogContent>
+
+    </DialogContent>
+    <DialogFooter>
+            <ConfirmButton>Add to order</ConfirmButton>
+    </DialogFooter>
     </Dialog>
     </>
     ) : null
