1cbf59fd28d28d0e74a4c35580f99c0389232b0d
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-07-08 23:02:10 -0400

    dialog

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -7,15 +7,17 @@ const MenuStyled = styled.div`
     height: 1000px;
     margin: 0px 400px 50px 20px;
 `
-export function Menu(){
-    return(
+export function Menu({setOpenFood}){
+    return (
     <MenuStyled>
         {Object.entries(foods).map(([sectionName, foods]) => (
             <>
         <h1>{sectionName}</h1>
         <FoodGrid>
-        {foods.map(food =>(
-            <Food img={food.img}>
+        {foods.map(food => (
+            <Food img={food.img} onClick={()=>{
+                setOpenFood(food);
+            }}>
                 <FoodLabel>{food.name}</FoodLabel>
             </Food>
         ))}
